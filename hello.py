d87e2e164953325e687639366ac6823f9be0693a
Peter Schiks <pjschiks@ziggo.nl>
2023-06-25 11:30:27 +0200

    Better search page, created basic admin page and standard profile pic

--- hello.py
+++ hello.py
@@ -153,6 +153,7 @@ def update(id):
         name_to_update.name = request.form["name"]
         name_to_update.email = request.form["email"]
         name_to_update.favorite_color = request.form["favorite_color"]
+        name_to_update.about_author = request.form["about_author"]
         name_to_update.username = request.form["username"]
         try:
             db.session.commit()
@@ -327,6 +328,7 @@ def dashboard():
         name_to_update.email = request.form["email"]
         name_to_update.favorite_color = request.form["favorite_color"]
         name_to_update.username = request.form["username"]
+        name_to_update.about_author = request.form["about_author"]
         try:
             db.session.commit()
             flash("User updated successfully!")
@@ -362,6 +364,16 @@ def search():
                                searched = post.searched,
                                posts=posts)
 
+@app.route('/admin')
+@login_required
+def admin():
+    id = current_user.id
+    if id == 9:
+        return render_template("admin.html")
+    else:
+        flash("You are not god enough to see this page. Go back you peasant.")
+        return redirect(url_for('posts'))
+
 # Create blog post model
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -380,6 +392,7 @@ class Users(db.Model, UserMixin):
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     favorite_color = db.Column(db.String(120))
+    about_author = db.Column(db.Text(500), nullable=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     # Do some password stuff
     password_hash = db.Column(db.String(128))
--- webforms.py
+++ webforms.py
@@ -1,6 +1,6 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, Length
 from wtforms.widgets import TextArea
 from flask_ckeditor import CKEditorField
@@ -26,6 +26,7 @@ class UserForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     favorite_color = StringField("Favorite color")
+    about_author = TextAreaField("About author")
     password_hash = PasswordField("Password", validators=[DataRequired(), EqualTo('password_hash2', message='password must match')])
     password_hash2 = PasswordField("Confirm password", validators=[DataRequired()])
     submit = SubmitField("Submit")
